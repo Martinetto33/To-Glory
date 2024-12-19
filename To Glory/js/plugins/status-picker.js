bd1cc04bb1270370576d776979cd7d66d5346dbc
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-12-19 11:10:00 +0100

    refactor: split function for detecting negative states in more functions, that now actually should only take negative states into consideration

--- To Glory/js/plugins/status-picker.js
+++ To Glory/js/plugins/status-picker.js
@@ -3,6 +3,12 @@
  * @plugindesc Plugin used to extract statuses from actors
  */
 
+// Array of negative statuses
+var negativeStatuses = [
+    3, // poison (id taken from database)
+    6, // bleeding
+]
+
 /**
  * This function is here to help with the effect of the bezoar,
  * which removes a random status from one of the players.
@@ -25,13 +31,32 @@ function selectRandomStatusIDFromPartyMember() {
     if (partyMember && partyMember.isActor()) {
         console.log(`Actor using the item: ${partyMember.name()}`)
     }
-    let partyMemberStates = partyMember._states
+    let allPartyMemberStates = partyMember._states
         .filter(state => state) // checks if state is not null
+    // This array will only contain the negative states.
+    let partyMemberStates = filterAllNegativeStates(allPartyMemberStates, partyMember.name())
     // Selecting a random status
     if (partyMemberStates.length > 0) {
         let index = getRandomInt(partyMemberStates.length)
-        partyMember.removeState(partyMemberStates[index].id)
+        let id = partyMemberStates[index].id
+        partyMember.removeState(id)
+        console.log(`Removed state ${id} from ${partyMember.name()}!`)
     } else {
         console.log(`No states found for actor ${partyMember.name()}`)
     }
 }
+
+function filterAllNegativeStates(array, actorName) {
+    partyMemberNegativeStates = []
+    for (i = 0; i < array.length; i++) {
+        if (isNegativeState(array[i].id)) {
+            console.log(`Actor ${actorName} is affected by negative status ${array[i].id}!`)
+            partyMemberNegativeStates.push(allPartyMemberStates[i])
+        }
+    }
+    return partyMemberNegativeStates
+}
+
+function isNegativeState(stateId) {
+    return negativeStatuses.includes(stateId)
+} 
