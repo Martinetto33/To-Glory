2ba86cbc47dac744f2a239f2d9800d2917e25f2d
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2025-01-21 21:12:55 +0100

    feat: implemented item limits for armor and weapons

--- To Glory/js/plugins/CustomShop.js
+++ To Glory/js/plugins/CustomShop.js
@@ -15,18 +15,56 @@
  * - if the shop is re-entered, sold out items could be left behind, but maybe this is too much work.
  */
 
-const blacksmithItemLimits = {
-
-};
-
-const merchantItemLimits = {
-
-};
+const blacksmithItemLimits = [
+    {
+        "id": 1,
+        "type": "weapon",
+        "name": "Tranquilizing Crossbow",
+        "limit": 2
+    },
+    {
+        "id": 2,
+        "type": "weapon",
+        "name": "Flaming Sword",
+        "limit": 2
+    },
+    {
+        "id": 3,
+        "type": "weapon",
+        "name": "Lightning Katars",
+        "limit": 3
+    },
+    {
+        "id": 1,
+        "type": "armor",
+        "name": "Chainmail",
+        "limit": 4
+    },
+    {
+        "id": 2,
+        "type": "armor",
+        "name": "Impenetrable Armor",
+        "limit": 4
+    },
+    {
+        "id": 3,
+        "type": "armor",
+        "name": "Mystical Shell",
+        "limit": 4
+    },
+];
 
 (() => {
-    const _Scene_Shop_doBuy = Scene_Shop.prototype.doBuy
-    Scene_Shop.prototype.doBuy = function(itemsNumber) {
-        console.log("[CUSTOM SHOP]: buying " + itemsNumber.toString() + " items!")
-        _Scene_Shop_doBuy.call(this, itemsNumber)
+    Game_Party.prototype.maxItems = function(item) {
+        // console.log("[CUSTOM SHOP]: max items called for item " + JSON.stringify(item))
+        if (item.atypeId) {
+            // console.log("This is an armor! Id = " + item.id + ", name = " + item.name)
+            return blacksmithItemLimits.find(it => it.type === "armor" && it.id === item.id).limit
+        } else if (item.wtypeId) {
+            // console.log("This is a weapon! Id = " + item.id + ", name = " + item.name)
+            return blacksmithItemLimits.find(it => it.type === "weapon" && it.id === item.id).limit
+        }
+        return 99
     }
 })()
+
