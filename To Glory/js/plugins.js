c3473e35dc50b5b77c1acf43ac477583042a7eb1
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2025-01-24 11:35:07 +0100

    feat: added back buttons to menus

--- To Glory/js/plugins.js
+++ To Glory/js/plugins.js
@@ -12,5 +12,6 @@ var $plugins =
 {"name":"CustomChoicesWindow","status":true,"description":"A window that displays some choices, and has an area to display some text next to the choices.","parameters":{}},
 {"name":"EnemySkills","status":true,"description":"Plugin containing custom enemies effects.","parameters":{}},
 {"name":"HealthBar","status":true,"description":"Plugin to display enemies health bars.","parameters":{}},
-{"name":"CustomShop","status":true,"description":"A way to add item limits to the shop.","parameters":{}}
+{"name":"CustomShop","status":true,"description":"A way to add item limits to the shop.","parameters":{}},
+{"name":"Bgs_MenusCancelCommand","status":true,"description":"Add a cancel command to menus of your choice","parameters":{"Cancel Command Name":"← Back","Include in Main Menu":"true","Include in Item":"true","Include in Skill":"true","Include in Equip":"true","Include in Status":"true","Include in Options":"true","Include in Save":"true","Include in Party":"true","Include in Shop":"true"}}
 ];
